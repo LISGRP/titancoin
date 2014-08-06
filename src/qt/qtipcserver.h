6cc1a1cd3a1eca8dce7fc0ecdac41b42ff911f71
LISGRP <lisinvestmentgroup@hotmail.com>
2014-08-06 18:29:46 -0400

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Titancoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "TitancoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
